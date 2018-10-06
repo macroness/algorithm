1c283362b6643a637514a1f37fcefe93caca8454
macroness <macroness@gmail.com>
2018-10-06 22:47:28 +0900

    [20181006] codeForce 479_C

---
+++ CodeForces/479_C_Exams.py
@@ -0,0 +1,24 @@
+from operator import itemgetter
+import sys
+n = int(input())
+arr = []
+
+for _ in range(n):
+	ab = list(map(int, input().split()))
+	arr.append(ab)
+
+
+arr.sort(key=itemgetter(0,1))
+
+if n == 1:
+    print (min(arr[0]))
+    sys.exit()
+
+minn = min(arr[0])
+for a in arr:
+    if minn <= a[1]:
+        minn = a[1]
+    else:
+        minn = a[0]
+
+print(minn)

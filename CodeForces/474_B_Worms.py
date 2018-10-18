f53fe30fa5d4427d406b74f6164ffb8cf8ea4a59
macroness <macroness@gmail.com>
2018-10-18 23:06:51 +0900

    [20181018] 474 B Worms 파이썬으로 한것(시간초과뜸...)

---
+++ CodeForces/474_B_Worms.py
@@ -0,0 +1,24 @@
+n = int(input())
+listA = list(map(int,input().split()))
+mm = int(input())
+listM = list(map(int,input().split()))
+
+for i in range(1,n):
+    listA[i] = listA[i] + listA[i-1]
+
+listA.insert(0, 0)
+
+def getPos(l, u, x):
+    m = int((l + u) / 2)
+
+    if listA[m] >= x and listA[m-1] < x:
+        print(m)
+        return
+
+    if listA[m] > x:
+        return getPos(l, m-1, x)
+    else:
+        return getPos(m+1, u, x)
+
+for a in listM:
+    getPos(0, n, a)
